bbf38991fd4c2281e8c7854fbc69024851a5237f
Makenna Wold <Makenna@Kidss-MacBook-Pro.local>
2018-10-05 14:38:59 -0700

    got our landing styles done

--- src/components/landing/input.js
+++ src/components/landing/input.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 const Input = (state, func) => {
         return (
             <div className="input">
-                <input className="input__monsterInput" placeholder="Whats Spookin You?" value={state} onChange={func}/>
+                <input className="input__monsterInput" placeholder="What's Spookin You?" value={state} onChange={func}/>
                 
             </div>
             
