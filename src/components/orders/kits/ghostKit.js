e8c034371981013dea3aab7aeee48317f65a3a6a
Carson Beus <carson.beus@gmail.com>
2018-10-05 14:40:14 -0600

    Final touches added with specific kits for specific monsters

--- src/components/orders/kits/ghostKit.js
+++ src/components/orders/kits/ghostKit.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
 class GhostKit extends Component {
   render() {
     return(
-        <div>
-            Ghost
-        </div>
+      <div className="kit__print__out">
+        <h1>Your Ghost Removal Kit</h1>
+        <h3>This kit contains the following:</h3>
+        <ol>
+          <li>Bible (King James Version)</li>
+          <li>Photon Blaster</li>
+          <li>Ectoplasm Collector</li>
+        </ol>
+      </div>
     )
   }
 }
--- src/components/orders/kits/zombieKit.js
+++ src/components/orders/kits/zombieKit.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
 class ZombieKit extends Component {
   render() {
     return(
-        <div>
-            ZombieKit
-        </div>
+      <div className="kit__print__out">
+        <h1>Your Zombie Removal Kit</h1>
+        <h3>This kit contains the following:</h3>
+        <ol>
+          <li>Shotgun</li>
+          <li>Baseball bat with nails</li>
+          <li>Machete</li>
+        </ol>
+      </div>
     )
   }
 }
