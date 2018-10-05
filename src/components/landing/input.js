fb506f3c887674917af71ff30a7a3845d11918bb
eidanfili <eidan@ledezma.org>
2018-10-05 11:12:12 -0600

    put content on the landing page and made the main input

--- src/components/landing/input.js
+++ src/components/landing/input.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 
-const Input = {
-    render() {
-        return (
-            <div className="input">
-
-            </div>
-        )
-    }
+const Input = () => {
+    return (
+        <div className="input">
+            <input className="input__monsterInput" placeholder="Whats Spookin You?"/>
+        </div>
+    )
 }
 export default Input;
--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import Input from './input';
 
 export default class Landing extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
+        <h1>Spooks Be Gone!</h1>
+        <h2>Whats Spookin you?</h2>
+        <div> 
+          <img src=""/>
+          got a vampire on your neck? we've got a kit for you!
+          <br></br>
+          <img src=""/>
+          if you have a werewolf hunting your kids? we have a kit a for that!
+          <br></br>
+          <img src=""/>
+          ghost watching you sleep? we have a solution for you!
+          <br></br>
+          <img src=""/>
+          zombies banging your door down? our newest kit has a solution for you!
+          </div>
+          <Input/>
       </div>
     );
   }
